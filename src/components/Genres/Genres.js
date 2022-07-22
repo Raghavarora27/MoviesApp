5b984ddb3359032c820c781253c866f6326cf386
Raghav Arora <66276244+Raghavarora27@users.noreply.github.com>
2022-07-22 20:56:00 +0530

    Update Genres.js

--- src/components/Genres/Genres.js
+++ src/components/Genres/Genres.js
@@ -26,7 +26,7 @@ const Genres = ({
 
   const fetchGenres = async () => {
     const { data } = await axios.get(
-      `https://api.themoviedb.org/3/genre/${type}/list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+      `https://api.themoviedb.org/3/genre/${type}/list?api_key=a60851b2cf31724f7bed307fdbb59c78&language=en-US`
     );
     setGenres(data.genres);
   };
